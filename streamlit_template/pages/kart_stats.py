ebc39aec4e127e693add078d38ecefe5b084362d
Dongdi Zhao <dongdizhao@gmail.com>
2024-02-05 22:28:22 -0700

    Complete Walkthrough

---
+++ streamlit_template/pages/kart_stats.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.markdown("# Kart Configurations 🏎️")
+st.sidebar.markdown("# Kart Configurations 🏎️")
+
+st.write("What Kart Configuration is Best?")
+
+df_kart = pd.read_csv('data/kart_stats.csv')
